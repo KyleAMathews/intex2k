9f1e58e1452fa6d1118ebd368f49b9e770e0d799
tylerfarmer <tylerfarmer@0693ecae-c32c-0410-81d0-bfa53a6e958f>
2007-04-02 20:04:53 +0000

    ""

---
+++ Intex2/src/edu/byu/isys413/cbb54/intex2/data/printFormat.java
@@ -0,0 +1,77 @@
+/*
+ * printFormat.java
+ *
+ * Created on March 30, 2007, 2:45 PM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package edu.byu.isys413.cbb54.intex2.data;
+
+/**
+ *
+ * @author kyle
+ */
+public class printFormat {
+    
+    private String id;
+    private String size;
+    private String paperType;
+    private String sourceType;
+    private boolean inDB;
+    private boolean dirty;
+    
+    /**
+     * Creates a new instance of printFormat
+     */
+    public printFormat(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+        this.dirty = true;
+    }
+
+    public String getPaperType() {
+        return paperType;
+    }
+
+    public void setPaperType(String paperType) {
+        this.paperType = paperType;
+        this.dirty = true;
+    }
+
+    public String getSourceType() {
+        return sourceType;
+    }
+
+    public void setSourceType(String sourceType) {
+        this.sourceType = sourceType;
+        this.dirty = true;
+    }
+
+    public boolean isInDB() {
+        return inDB;
+    }
+
+    public void setInDB(boolean inDB) {
+        this.inDB = inDB;
+    }
+
+    public boolean isDirty() {
+        return dirty;
+    }
+    
+}
+
+// take it away Tyler
