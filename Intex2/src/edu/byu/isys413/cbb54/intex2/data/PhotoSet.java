e5c067e99a5a3ef9138387fbff5a6456085b3a20
tylerfarmer <tylerfarmer@0693ecae-c32c-0410-81d0-bfa53a6e958f>
2007-04-02 20:43:08 +0000

    ""

--- Intex2/src/edu/byu/isys413/cbb54/intex2/data/PhotoSet.java
+++ Intex2/src/edu/byu/isys413/cbb54/intex2/data/PhotoSet.java
@@ -13,10 +13,55 @@ package edu.byu.isys413.cbb54.intex2.data;
  *
  * @author kyle
  */
-public class PhotoSet {
+public class PhotoSet{
+    
+    private String id;
+    private String description;
+    private int numPhotos;
+    private boolean dirty = false;
+    private boolean inDB = false;
     
     /** Creates a new instance of PhotoSet */
-    public PhotoSet() {
+    public PhotoSet(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+        this.setDirty(true);
+    }
+
+    public int getNumPhotos() {
+        return numPhotos;
+    }
+
+    public void setNumPhotos(int numPhotos) {
+        this.numPhotos = numPhotos;
+        this.setDirty(true);
+    }
+
+    public boolean getIsDirty() {
+        return dirty;
+    }
+
+    public boolean getIsInDB() {
+        return inDB;
+    }
+
+    public void setInDB(boolean inDB) {
+        this.inDB = inDB;
+    }
+
+    public void setDirty(boolean dirty) {
+        this.dirty = dirty;
     }
     
 }
